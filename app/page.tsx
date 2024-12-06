7d6b392399d7f8e410eb3ea8fd90d749a90f4f83
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-06 11:21:16 -0800

    navabar coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/Hero";
+import { Navbar } from "@/components/Navbar";
 import { ServiceWorkerRegistration } from "@/components/ServiceWorkerRegistration";
 
 export default function HomePage() {
   return (
     <div>
+      {/* <Navbar /> */}
       <Hero />
       <ServiceWorkerRegistration />
     </div>
