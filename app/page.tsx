70c5a40c77f009b5ec6ae2c11039ace0818938eb
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-06 11:26:01 -0800

    bye navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { Hero } from "@/components/Hero";
-import { Navbar } from "@/components/Navbar";
 import { ServiceWorkerRegistration } from "@/components/ServiceWorkerRegistration";
 
 export default function HomePage() {
   return (
     <div>
-      {/* <Navbar /> */}
       <Hero />
       <ServiceWorkerRegistration />
     </div>
