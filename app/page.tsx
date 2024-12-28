35a5c2674b908d7ba9aa714079e3e9a0cd2cc774
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2024-12-28 14:27:34 -0800

    build issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { Hero } from "@/components/Hero";
 import { ServiceWorkerRegistration } from "@/components/ServiceWorkerRegistration";
 import { Navbar } from "@/components/navbar";
-import { BackgroundBeams } from "@/components/ui/background-beams";
+// import { BackgroundBeams } from "@/components/ui/background-beams";
 
 
 export default function HomePage() {
   return (
     <div className="absolute inset-0 w-full h-full bg-gradient-to-b from-white to-blue-100" >
       <Navbar />
-      <BackgroundBeams className="absolute z-[-5]" />
+      {/* <BackgroundBeams /> */}
       <Hero />
       <ServiceWorkerRegistration />
     </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { BackgroundBeams } from "@/components/ui/background-beams";
+// import { BackgroundBeams } from "@/components/ui/background-beams";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 export function Hero() {
