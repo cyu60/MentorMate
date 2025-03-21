b842e35621a7cc0e69db01ea3d43e7eb452027c7
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-03-20 20:58:52 -0700

    grid updates

--- components/projects/ProjectBoard/ProjectBoard.tsx
+++ components/projects/ProjectBoard/ProjectBoard.tsx
@@ -44,7 +44,7 @@ const ProjectBoard = ({ isLoading, projectList, session, projectBoardContext }: 
                     className="mx-auto"
                 >
                     {projectList.length > 0 ? (
-                        <div className="grid grid-cols-3 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                             {projectList.map((project) => (
                                 <Card key={project.id} className="overflow-hidden shadow-lg">
                                     {/* Cover Image */}
