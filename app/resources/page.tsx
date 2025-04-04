5a63f15902137bdef3d96268bd0ad57d3a1a46f4
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-04-04 16:03:11 -0700

    page set up

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,11 +1,24 @@
-export default function Page() {
+
+import { AuthNavbar } from "@/components/layout/authNavbar";
+import { Footer } from "@/components/layout/footer";
+
+export default function Resources() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold mb-4">Welcome to the App</h1>
-        <p className="text-lg mb-8">This is a sample page.</p>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded">
-            Click Me
-        </button>
+    <div className="min-h-full bg-gray-50 pt-10 text-center">
+            <AuthNavbar />
+            
+            <div className="text-center mb-12">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-blue-900">
+                    Resources
+                </h1>
+                <p className="mt-2 text-gray-600">
+                    Check out these resources to help you get started with your projects.
+                </p>
+            </div>
+
+            <div className="absolute bottom-0 w-full">   
+                <Footer />
+            </div>
         </div>
     );
 }
